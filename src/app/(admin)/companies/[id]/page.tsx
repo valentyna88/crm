f219be40ece87dbc7bb67d65943d10055663b7a6
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-02-17 19:23:38 +0200

    feat: Add error handling components

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -5,7 +5,7 @@ export interface PageProps {
   params: { id: string };
 }
 
-export default async function Page({ params }: PageProps) {
+export default function Page({ params }: PageProps) {
   return (
     <>
       <Header>Company ({params.id})</Header>
