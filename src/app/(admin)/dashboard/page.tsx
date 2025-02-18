aaca1390a6a79f96328d9fe4301caf083b140ed3
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-02-18 15:25:45 +0200

    feat: Add dashboard slots

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,11 +1,12 @@
+import Header from '@/app/components/header';
 import React from 'react';
 
 // export interface PageProps {}
 
 export default function Page() {
   return (
-    <main>
-      <h1 className="text-xl">Dashboard Page</h1>
-    </main>
+    <>
+      <Header>Dashboard</Header>
+    </>
   );
 }
