ec9279f56eab8e3b1bb3272883e6601271ecade3
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-02-18 17:03:17 +0200

    feat: Add new company page

---
+++ src/app/(admin)/companies/@modal/(.)new/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import CompanyForm from '@/app/components/company-form';
+import Modal from '@/app/components/modal';
+
+// export interface PageProps { }
+
+export default function Page() {
+  const router = useRouter();
+  return (
+    <Modal show={true} onClose={() => router.back()}>
+      <CompanyForm onSubmit={console.log} />
+    </Modal>
+  );
+}
